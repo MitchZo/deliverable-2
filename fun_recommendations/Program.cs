a528d03d98d67fb138576842fba434489243c5e6
Mitch <outvisible@gmail.com>
2019-05-11 19:00:49 -0400

    first Commit

---
+++ fun_recommendations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fun_Recommendations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fun Recommendations!");
+        }
+    }
+}
